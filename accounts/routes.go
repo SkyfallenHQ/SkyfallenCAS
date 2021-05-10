5fa91fa4a1eb93c5ddf761aab0d90be568ffeebd
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-05-10 22:48:57 +0300

    Setup accounts package routes

---
+++ accounts/routes.go
@@ -0,0 +1,22 @@
+package accounts
+
+import (
+	"backend/structures"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Route(app structures.SkyfallenCASApp){
+
+	app.WA.Post("/accounts/create", func(ctx *fiber.Ctx) error {
+
+		return createAccount(ctx, app)
+
+	})
+
+	app.WA.Post("/accounts/login", func(ctx *fiber.Ctx) error {
+
+		return login(ctx, app)
+
+	})
+
+}
